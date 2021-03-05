d47b5d73aaef91d2b36c49bf6a3e6b9557dff2b6
daria <tochdz@gmail.com>
2021-03-05 16:34:36 +0300

    animation added

--- src/.vuepress/components/Acc.vue
+++ src/.vuepress/components/Acc.vue
@@ -37,12 +37,13 @@ section
     max-width var(--sectionWidth)
     margin 3rem auto
     width 97%
-    color #000
+    // color #000
 
 summary
     display block
     cursor pointer
-    padding 10px
+    outline none 
+    padding 0.5rem
     transition .3s
     // border-bottom 1px solid #d5d3d6 
     user-select none
@@ -54,21 +55,58 @@ details
         overflow auto
         height 100%
         user-select none
-        padding 0 20px
+        padding 0 1.25rem
         line-height 1.5
+        border 1px solid #d5d3d6 
         & > img
             align-self flex-start
             max-width 100%
-            margin-top 20px
+            margin-top 1.25rem
         & > p
             flex 1
 
 details[open]
     & > summary
         color #2E26D9
-        border-bottom 1px solid #2E26D9
+        // border-bottom 1px solid #2E26D9
     
+details[open] > div {
+    animation-name slideDown
+    animation-duration 0.3s
+    animation-fill-mode forwards
 
+  }
+details[open] > div > p {
+    opacity: 0;
+    animation-name: showContent;
+    animation-duration: 0.6s;
+    animation-delay: 0.2s;
+    animation-fill-mode: forwards;
+    margin: 0;
+    padding-left: 20px;
+  }
+@keyframes slideDown {
+  from {
+    opacity: 0;
+    height: 0;
+    padding: 0;
+  }
+
+  to {
+    opacity: 1;
+    height: var(--contentHeight);
+    padding: 20px;
+  }
+}
+
+@keyframes showContent {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
 
 
 </style>
